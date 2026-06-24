3486b4c228e8f7760dd2278934ba630df98c01eb
jcx10ten <juancamiloanaya10@gmail.com>
2026-06-23 22:05:40 -0500

    Controller

---
+++ Activity2_Notes_Administrator/src/co/edu/udistrital/controller/Controller.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.udistrital.controller;
+
+import co.edu.udistrital.model.Estudiante;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Controller {
+    
+    private List<Estudiante> estudiantes;
+    
+    public Controller() { 
+        estudiantes = new ArrayList<>();
+    }
+    
+    
+    
+}
--- Activity2_Notes_Administrator/src/co/edu/udistrital/model/Estudiante.java
+++ Activity2_Notes_Administrator/src/co/edu/udistrital/model/Estudiante.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package co.edu.udistrital.model;
 
-/**
- *
- * @author USUARIO
- */
 public class Estudiante {
+    
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String nota;
+    private String codigo;
+
+    public Estudiante(String nombre, String apellido, String cedula, String nota, String codigo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.nota = nota;
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+    
+    
     
 }
